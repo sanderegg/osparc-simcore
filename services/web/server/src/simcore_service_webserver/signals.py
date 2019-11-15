1903d7d85965e37d853d58a75ecd2c64f8465656
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-11-15 08:15:19 +0100

    refactoring

--- services/web/server/src/simcore_service_webserver/signals.py
+++ services/web/server/src/simcore_service_webserver/signals.py
@@ -1,13 +1,19 @@
 import asyncio
 import logging
 from collections import defaultdict
+from enum import Enum
 from functools import wraps
 
 log = logging.getLogger(__name__)
 
 event_registry = defaultdict(list)
 
-async def emit(event: str, *args, **kwargs):
+class SignalName(Enum):
+    SIGNAL_USER_CONNECT = 1
+    SIGNAL_USER_DISCONNECT = 2
+
+
+async def emit(event: SignalName, *args, **kwargs):
     if not event_registry[event]:
         return
 
@@ -15,7 +21,7 @@ async def emit(event: str, *args, **kwargs):
     # all coroutine called in //
     await asyncio.gather(*coroutines, return_exceptions=True)
 
-def observe(event: str):
+def observe(event: SignalName):
     def decorator(func):
         if func not in event_registry[event]:
             event_registry[event].append(func)
