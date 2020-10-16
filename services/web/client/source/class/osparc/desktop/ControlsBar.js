020ee1be911afb847c36017aab37e9ffcf9ef813
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-10-16 15:53:15 +0200

    implement cancelling task

--- services/web/client/source/class/osparc/desktop/ControlsBar.js
+++ services/web/client/source/class/osparc/desktop/ControlsBar.js
@@ -47,7 +47,8 @@ qx.Class.define("osparc.desktop.ControlsBar", {
     "showSettings": "qx.event.type.Event",
     "groupSelection": "qx.event.type.Event",
     "ungroupSelection": "qx.event.type.Event",
-    "startPipeline": "qx.event.type.Event"
+    "startPipeline": "qx.event.type.Event",
+    "stopPipeline": "qx.event.type.Event"
   },
 
   members: {
@@ -118,6 +119,8 @@ qx.Class.define("osparc.desktop.ControlsBar", {
       this.add(moreCtrls);
 
       const simCtrls = new qx.ui.toolbar.Part();
+      const stopButton = this.__createStopButton();
+      simCtrls.add(stopButton);
       const startButton = this.__createStartButton();
       simCtrls.add(startButton);
       this.add(simCtrls);
@@ -161,6 +164,10 @@ qx.Class.define("osparc.desktop.ControlsBar", {
     __createStartButton: function() {
       const startButton = this.__startButton = this.__createButton(this.tr("Run"), "play", "runStudyBtn", "startPipeline");
       return startButton;
+    },
+    __createStopButton: function() {
+      const stopButton = this.__stopButton = this.__createButton(this.tr("Stop"), "stop", "stopStudyBtn", "stopPipeline");
+      return stopButton;
     },
 
     __createRadioButton: function(label, icon, widgetId, singalName) {
