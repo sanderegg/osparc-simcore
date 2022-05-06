7cc422b7678d73fb4ebc977b4dbd67cbf5c19707
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-06 16:16:12 +0200

    fix creation of client

--- services/storage/tests/unit/test_s3.py
+++ services/storage/tests/unit/test_s3.py
@@ -1,17 +1,19 @@
-# pylint: disable=protected-access
+from aiohttp.test_utils import TestClient
+from simcore_service_storage.s3 import get_s3_client
+from simcore_service_storage.settings import Settings
 
-import pytest
-from simcore_service_storage import s3
+pytest_simcore_core_services_selection = ["postgres"]
+pytest_simcore_ops_services_selection = ["minio", "adminer"]
 
 
-@pytest.mark.parametrize(
-    "url, expected",
-    [
-        ("https://ceph.com", "ceph.com"),
-        ("http://ciao.com", "ciao.com"),
-        ("http://local.address:8012", "local.address:8012"),
-        ("https://remote.stragen.com:4432", "remote.stragen.com:4432"),
-    ],
-)
-def test_minio_client_endpint(url: str, expected: str) -> None:
-    assert s3._minio_client_endpint(url) == expected
+async def test_s3_client(app_settings: Settings, client: TestClient):
+    assert client.app
+    s3_client = get_s3_client(client.app)
+    assert s3_client
+
+    response = await s3_client.list_buckets()
+    assert response
+    assert "Buckets" in response
+    assert len(response["Buckets"]) == 1
+    assert "Name" in response["Buckets"][0]
+    assert response["Buckets"][0]["Name"] == app_settings.STORAGE_S3.S3_BUCKET_NAME
