c06b79ad79851832260b02f8cd6392e4e0074b28
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-06 16:26:44 +0200

    check if bucket is already created

--- services/storage/tests/unit/test_s3.py
+++ services/storage/tests/unit/test_s3.py
@@ -1,12 +1,30 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+
+import pytest
 from aiohttp.test_utils import TestClient
 from simcore_service_storage.s3 import get_s3_client
 from simcore_service_storage.settings import Settings
+
+from services.storage.tests.helpers.s3_client import MinioClientWrapper
 
 pytest_simcore_core_services_selection = ["postgres"]
 pytest_simcore_ops_services_selection = ["minio", "adminer"]
 
 
-async def test_s3_client(app_settings: Settings, client: TestClient):
+@pytest.fixture(params=[True, False])
+def pre_created_bucket(app_settings: Settings, s3_client: MinioClientWrapper, request):
+    if request.param:
+        s3_client.create_bucket(
+            app_settings.STORAGE_S3.S3_BUCKET_NAME, delete_contents_if_exists=True
+        )
+
+
+async def test_s3_client(
+    pre_created_bucket: None, app_settings: Settings, client: TestClient
+):
     assert client.app
     s3_client = get_s3_client(client.app)
     assert s3_client
