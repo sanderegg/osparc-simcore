ebc69874bc447f6411cf23286d739b946d327a74
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-06 11:19:52 +0200

    use aiobotocore in src

--- services/storage/src/simcore_service_storage/s3.py
+++ services/storage/src/simcore_service_storage/s3.py
@@ -3,64 +3,67 @@
 """
 import logging
 
+from aiobotocore.session import get_session
 from aiohttp import web
-from pydantic import AnyUrl, parse_obj_as
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from botocore.client import Config
+from tenacity._asyncio import AsyncRetrying
+from tenacity.before_sleep import before_sleep_log
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_fixed
 
 from .constants import APP_CONFIG_KEY, APP_S3_KEY
-from .s3wrapper.s3_client import MinioClientWrapper
-from .settings import Settings
-from .utils import RETRY_COUNT, RETRY_WAIT_SECS
+from .utils import MINUTE, RETRY_WAIT_SECS
 
 log = logging.getLogger(__name__)
 
+_APP_S3_SESSION = f"{__name__}.s3_session"
 
-async def _setup_s3_bucket(app):
+
+async def setup_s3_client(app):
     log.debug("setup %s.setup.cleanup_ctx", __name__)
-
     # setup
-    s3_client = app[APP_S3_KEY]
-    cfg: Settings = app[APP_CONFIG_KEY]
+    storage_s3_settings = app[APP_CONFIG_KEY].STORAGE_S3
+    app[_APP_S3_SESSION] = session = get_session()
 
-    @retry(
+    async for attempt in AsyncRetrying(
         wait=wait_fixed(RETRY_WAIT_SECS),
-        stop=stop_after_attempt(RETRY_COUNT),
+        stop=stop_after_delay(2 * MINUTE),
         before_sleep=before_sleep_log(log, logging.WARNING),
         reraise=True,
-    )
-    async def do_create_bucket():
-        log.debug("Creating bucket: %s", cfg.STORAGE_S3.json(indent=2))
-        s3_client.create_bucket(cfg.STORAGE_S3.S3_BUCKET_NAME)
+    ):
+        with attempt:
+            app[APP_S3_KEY] = s3_client = session.create_client(
+                "s3",
+                endpoint_url=storage_s3_settings.S3_ENDPOINT,
+                aws_access_key_id=storage_s3_settings.S3_ACCESS_KEY,
+                aws_secret_access_key=storage_s3_settings.S3_SECRET_KEY,
+                config=Config(signature_version="s3v4"),
+            )
+            async with s3_client as client:
+                log.debug("Creating bucket: %s", storage_s3_settings.json(indent=2))
+                try:
+                    await client.create_bucket(
+                        Bucket=storage_s3_settings.S3_BUCKET_NAME
+                    )
+                    log.info(
+                        "Bucket %s successfully created [%s]",
+                        storage_s3_settings.S3_BUCKET_NAME,
+                        attempt.retry_state.retry_object.statistics,
+                    )
+                except client.exceptions.BucketAlreadyOwnedByYou:
+                    log.info(
+                        "Bucket %s already exists and is valid [%s]",
+                        storage_s3_settings.S3_BUCKET_NAME,
+                        attempt.retry_state.retry_object.statistics,
+                    )
 
-    try:
-        await do_create_bucket()
-    except Exception:  # pylint: disable=broad-except
-        log.exception("Impossible to create s3 bucket. Stoping")
-
-    # ok, failures_count = False, 0
-    # while not ok:
-    #     try:
-    #         s3_client.create_bucket(s3_bucket)
-    #         ok = True
-    #     except Exception: # pylint: disable=W0703
-    #         failures_count +=1
-    #         if failures_count>RETRY_COUNT:
-    #             log.exception("")
-    #             raise
-    #         await asyncio.sleep(RETRY_WAIT_SECS)
     yield
+    # if app[APP_S3_KEY]:
+    #     s3_client = app[APP_S3_KEY]
+    #     await s3_client.close()
 
     # tear-down
     log.debug("tear-down %s.setup.cleanup_ctx", __name__)
-
-
-def _minio_client_endpint(s3_endpoint: str) -> str:
-    # Minio client adds http and https based on the secure paramenter
-    # provided at construction time, already including the schema
-    # will cause issues, encoding url to HOST:PORT or just HOST
-    # if port is missing
-    url = parse_obj_as(AnyUrl, s3_endpoint)
-    return f"{url.host}" if url.port is None else f"{url.host}:{url.port}"
 
 
 def setup_s3(app: web.Application):
@@ -73,19 +76,4 @@ def setup_s3(app: web.Application):
         log.warning("Service '%s' explicitly disabled in config", "s3")
         return
 
-    cfg = app[APP_CONFIG_KEY]
-
-    s3_client = MinioClientWrapper(
-        _minio_client_endpint(cfg.STORAGE_S3.S3_ENDPOINT),
-        cfg.STORAGE_S3.S3_ACCESS_KEY,
-        cfg.STORAGE_S3.S3_SECRET_KEY,
-        secure=cfg.STORAGE_S3.S3_SECURE,
-    )
-    app[APP_S3_KEY] = s3_client
-
-    app.cleanup_ctx.append(_setup_s3_bucket)
-
-
-def get_config_s3(app: web.Application) -> dict:
-    cfg = app[APP_CONFIG_KEY].STORAGE_S3
-    return cfg
+    app.cleanup_ctx.append(setup_s3_client)
