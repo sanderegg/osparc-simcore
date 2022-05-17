b112ef30293e57e5918a5371e54ae3b79c41d222
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-17 19:40:26 +0200

    upload_id added in file_meta_data

---
+++ services/storage/src/simcore_service_storage/db_file_meta_data.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from aiopg.sa.connection import SAConnection
+from models_library.users import UserID
+from simcore_postgres_database.models.file_meta_data import file_meta_data
+from sqlalchemy.dialects.postgresql import insert as pg_insert
+
+from .models import FileMetaData
+
+
+async def upsert_file_metadata_for_upload(
+    conn: SAConnection,
+    user_id: UserID,
+    bucket: str,
+    file_uuid: str,
+    upload_id: Optional[str],
+) -> FileMetaData:
+    parts = file_uuid.split("/")
+    if len(parts) != 3:
+        raise ValueError(f"{file_uuid=} does not follow conventions")
+
+    fmd = FileMetaData()
+    fmd.simcore_from_uuid(file_uuid, bucket)
+    fmd.user_id = user_id  # type: ignore
+    fmd.upload_id = upload_id  # type: ignore
+
+    # NOTE: upsert file_meta_data, if the file already exists, we update the whole row
+    # so we get the correct time stamps
+    insert_statement = pg_insert(file_meta_data).values(**vars(fmd))
+    on_update_statement = insert_statement.on_conflict_do_update(
+        index_elements=[file_meta_data.c.file_uuid], set_=vars(fmd)
+    )
+    await conn.execute(on_update_statement)
+
+    return fmd
