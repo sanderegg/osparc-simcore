2b6e372bdefeba6c5c59591e6897c540f5ea2f73
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-19 13:38:39 +0200

    added new fct for db access

--- services/storage/src/simcore_service_storage/db_file_meta_data.py
+++ services/storage/src/simcore_service_storage/db_file_meta_data.py
@@ -1,19 +1,22 @@
 from typing import Optional
 
+import sqlalchemy as sa
 from aiopg.sa.connection import SAConnection
 from models_library.users import UserID
 from simcore_postgres_database.models.file_meta_data import file_meta_data
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 
+from .exceptions import FileMetaDataNotFoundError
 from .models import FileMetaData
+from .s3 import FileID, UploadID
 
 
 async def upsert_file_metadata_for_upload(
     conn: SAConnection,
     user_id: UserID,
     bucket: str,
-    file_uuid: str,
-    upload_id: Optional[str],
+    file_uuid: FileID,
+    upload_id: Optional[UploadID],
 ) -> FileMetaData:
     parts = file_uuid.split("/")
     if len(parts) != 3:
@@ -33,3 +36,37 @@ async def upsert_file_metadata_for_upload(
     await conn.execute(on_update_statement)
 
     return fmd
+
+
+async def get_upload_id(conn: SAConnection, file_uuid: FileID) -> Optional[UploadID]:
+    return await conn.scalar(
+        sa.select([file_meta_data.c.upload_id]).where(
+            file_meta_data.c.file_uuid == file_uuid
+        )
+    )
+
+
+async def exists(conn: SAConnection, file_uuid: FileID) -> bool:
+    return bool(
+        await conn.scalar(
+            sa.select([file_meta_data.c.file_uuid]).where(
+                file_meta_data.c.file_uuid == file_uuid
+            )
+        )
+        == file_uuid
+    )
+
+
+async def delete(conn: SAConnection, file_uuid: FileID) -> None:
+    await conn.execute(
+        file_meta_data.delete().where(file_meta_data.c.file_uuid == file_uuid)
+    )
+
+
+async def get(conn: SAConnection, file_uuid: FileID) -> FileMetaData:
+    result = await conn.execute(
+        query=sa.select([file_meta_data]).where(file_meta_data.c.file_uuid == file_uuid)
+    )
+    if row := await result.fetchone():
+        return FileMetaData(**dict(row))  # type: ignore
+    raise FileMetaDataNotFoundError(file_uuid=file_uuid)
