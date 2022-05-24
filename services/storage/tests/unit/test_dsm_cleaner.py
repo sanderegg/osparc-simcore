cf078e87441abe03719511949a09e8f4e7441108
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-05-24 11:40:43 +0200

    test if tasks correctly started

--- services/storage/tests/unit/test_dsm_cleaner.py
+++ services/storage/tests/unit/test_dsm_cleaner.py
@@ -1,5 +1,15 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+import asyncio
+
 from aiohttp.test_utils import TestClient
 
+pytest_simcore_core_services_selection = ["postgres"]
+pytest_simcore_ops_services_selection = ["adminer"]
 
-def test_setup_dsm_cleaner(client: TestClient):
-    ...
+
+async def test_setup_dsm_cleaner(client: TestClient):
+    all_tasks = asyncio.all_tasks()
+    assert any(t.get_name().startswith("dsm_cleaner_task") for t in all_tasks)
